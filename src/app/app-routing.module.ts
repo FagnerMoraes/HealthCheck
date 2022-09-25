255cd8f5ba8050e70080d8de2e4d74362d2fd47f
Fagner Moraes <fagm.carvalho@gmail.com>
2022-09-25 13:22:11 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'fetch-data', component: FetchDataComponent }
+  { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'health-check', component: HealthCheckComponent }
+
 ];
 
 @NgModule({
